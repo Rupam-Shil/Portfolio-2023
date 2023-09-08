ef42063063ec128feb83c315bfd9ba4a8ca8affb
Rupam Shil <rupamshil111@gmail.com>
2023-09-08 21:48:47 +0530

    (FeaT) move to ts

---
+++ src/components/Background.tsx
@@ -0,0 +1,23 @@
+// import { Environment, Sphere } from '@react-three/drei';
+// import { Gradient, LayerMaterial } from 'lamina';
+// import React from 'react';
+// import * as THREE from 'three';
+
+// function Background() {
+// 	return (
+// 		<div>
+// 			<Environment preset="sunset" />
+// 			<Sphere scale={[100, 100, 100]} rotation-y={Math.PI / 2}>
+// 				<LayerMaterial
+// 					lighting="physical"
+// 					transmission={1}
+// 					side={THREE.BackSide}
+// 				>
+// 					<Gradient colorA={'blue'} colorB={'red'}></Gradient>
+// 				</LayerMaterial>
+// 			</Sphere>
+// 		</div>
+// 	);
+// }
+
+// export default Background;
